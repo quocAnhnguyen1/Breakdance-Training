a5ff466f9c989b5b592c7093177a93f26f1ae826
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2026-05-13 02:18:33 +0200

    add footworksession repository

---
+++ src/main/java/de/htw_berlin/mytodolist/FootworkSessionRepository.java
@@ -0,0 +1,6 @@
+package de.htw_berlin.mytodolist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FootworkSessionRepository extends JpaRepository<FootworkSession, Long> {
+}
